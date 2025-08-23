0dbd13078c7eefdd401e13041826242dd223228e
M1n9X <mingxue.mailbox@gmail.com>
2025-08-24 01:10:37 +0800

    chore(log): replace logging with loguru
    - Remove standard logging setup from multiple scripts
    - Add loguru as a dependency in pyproject.toml
    - Update import statements and logger initialization to use loguru

--- benchmarks/load_data.py
+++ benchmarks/load_data.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 import sys
 import os
-import logging
+from loguru import logger
 import asyncio
 import argparse
 
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler()],
-)
-logger = logging.getLogger(__name__)
 
 # Get the absolute path to the project root
 script_dir = os.path.dirname(os.path.abspath(__file__))
--- benchmarks/run_benchmark.py
+++ benchmarks/run_benchmark.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
-import logging
+from loguru import logger
 import asyncio
 import argparse
 
@@ -9,14 +9,6 @@ from dotenv import load_dotenv
 import plotext as plt
 
 load_dotenv(override=True)
-
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler()],
-)
-logger = logging.getLogger(__name__)
 
 # Get the absolute path to the project root
 script_dir = os.path.dirname(os.path.abspath(__file__))
